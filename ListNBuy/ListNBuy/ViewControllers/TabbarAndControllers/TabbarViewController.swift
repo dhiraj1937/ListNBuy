917687a19bc9197b0b882349718ee26e711f68e4
Rajesh Jayaswal <rajesh.jayaswal@smartsalary.com.au>
2021-01-15 15:37:47 +0530

    clean tabbar code

--- ListNBuy/ListNBuy/ViewControllers/TabbarAndControllers/TabbarViewController.swift
+++ ListNBuy/ListNBuy/ViewControllers/TabbarAndControllers/TabbarViewController.swift
@@ -8,13 +8,6 @@
 import UIKit
 
 class TabbarViewController: UITabBarController, UITabBarControllerDelegate {
-    
-    @IBOutlet var tabbarView: UIView!
-    @IBOutlet weak var item0: UIButton!
-    @IBOutlet weak var item1: UIButton!
-    @IBOutlet weak var item2: UIButton!
-    @IBOutlet weak var item3: UIButton!
-    @IBOutlet weak var item4: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,10 +34,7 @@ class TabbarViewController: UITabBarController, UITabBarControllerDelegate {
         
         self.viewControllers = [homeNVC , myOrdersNVC , offersNVC , wishlistNVC, myAccountNVC]
         self.selectedIndex = 0
-        self.item0.isSelected = true
         self.view.tintColor = UIColor.init(hexString: "#953A9D", alpha: 1)
-        self.tabbarView.frame = CGRect.init(x: 0, y: UIScreen.main.bounds.size.height - 94, width: UIScreen.main.bounds.size.width, height: 94)
-        //self.view.addSubview(self.tabbarView)
         Constant.globalTabbar = self
     }
     
@@ -52,40 +42,5 @@ class TabbarViewController: UITabBarController, UITabBarControllerDelegate {
     func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
         print("Should select viewController: \(viewController.title ?? "") ?")
         return true;
-    }
-    
-    @IBAction func tabItemPressed(_ sender: UIButton) {
-        reset()
-        self.selectedIndex = sender.tag
-        sender.isSelected = true
-    }
-    
-    func reset() {
-        self.item0.isSelected = false
-        self.item1.isSelected = false
-        self.item2.isSelected = false
-        self.item3.isSelected = false
-        self.item4.isSelected = false
-    }
-    
-//    func selectIndex0() {
-//        reset()
-//        self.selectedIndex = 0
-//        item0.isSelected = true
-//    }
-    
-
-}
-
-extension UIImage {
-    
-    class func imageWithColor(color: UIColor, size: CGSize) -> UIImage {
-        let rect: CGRect = CGRect(x: 0.0, y: 0.0, width: Double(size.width), height: Double(size.height))
-        UIGraphicsBeginImageContextWithOptions(size, false, 0)
-        color.setFill()
-        UIRectFill(rect)
-        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        return image
     }
 }
